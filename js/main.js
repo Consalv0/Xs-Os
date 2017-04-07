09bf93e004d309518fe36d8d72fbac57992a4407
Consalvo <gonzaloceciliano@gmail.com>
2017-04-07 00:51:46 -0500

    Erased First Push

--- js/main.js
+++
@@ -1,7 +0,0 @@
-/* eslint space-infix-ops: 0, space-before-function-paren: 0, indent: 0 */
-/* global $ */
-var navbarHeight = $('nav').outerHeight()
-$('html, body').css({
-  overflow: 'hidden',
-  height: '100%'
-})
