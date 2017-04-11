3676a5ee139eb005a84f5410553df915907a9576
Consalvo <gonzaloceciliano@gmail.com>
2017-04-11 00:24:48 -0500

    Redesigned

--- js/game.js
+++ js/game.js
@@ -1,42 +1,176 @@
 /* eslint space-infix-ops: 0, space-before-function-paren: 0, indent: 0, no-trailing-spaces: 0 */
 /* global $, createCanvas, loadFont, colorMode, blendMode, fill, text, textFont, textSize, DIFFERENCE, HSB, width,
-   ellipse, ellipseMode, RADIUS, CENTER, print, rect, rectMode, rotate, PI, noStroke, height, BLEND, push, translate,
-   pop, angleMode, DEGREES, mouseX, mouseY */
+   ellipse, ellipseMode, RADIUS, rect, rectMode, rotate, noStroke, height, BLEND, push, translate,
+   pop, angleMode, DEGREES, mouseX, mouseY, CENTER */
 
-let outerGrid = new Grid({
-  lvl: 0,
-  width: 3,
-  height: 3,
-  values: [0, 0, 0,
-           0, 0, 0,
-           0, 0, 0]
-})
+// let UbuntuMono
+let grids = []
+let mouseIsCliked = false
+let zoom
+let clock = 0
+let GRIDSIZE = 2
+let LEVELS = 5
 
-let innerGrid = new Grid({
-  lvl: 1,
-  width: 3,
-  height: 3,
-  values: [1, 1, 2,
-           2, 2, 1,
-           2, 1, 1]
-})
+// function Grid(options) {
+//   this.lvl = LEVELS
+//   this.width = width
+//   this.height = height
+//   // this.values = []
+//
+//   if (options) {
+//     for (let prop in options) {
+//       if (this.hasOwnProperty(prop)) {
+//         this[prop] = options[prop]
+//       }
+//     }
+//   }
+//
+//   this.values = []
+//   for (let i = 0; i < GRIDSIZE**2; i++) {
+//     if (this.lvl <= LEVELS-1) {
+//       // this.values.push(Math.ceil(Math.random()*3))
+//       this.values.push(3)
+//     } else {
+//       this.values.push(Math.floor(Math.random()*3))
+//     }
+//   }
+// }
+//
+// Grid.prototype = {
+//
+//   draw: function(lvl, num) {
+//     this.__drawSharp(this._posX(), this._posY(), this._size())
+//     for (var i = 0; i < this.values.length; i++) {
+//       if (this.values[i] === 1) {
+//         this.__drawX(this._posX(), this._posY(), this._size())
+//       }
+//       if (this.values[i] === 2) {
+//         this.__drawO(this._posX(), this._posY(), this._size())
+//       }
+//     }
+//   },
+//
+//   _posX: function() {
+//     let posX
+//     let colum
+//     colum = this.num-GRIDSIZE*Math.floor(this.num/GRIDSIZE)
+//     posX = colum *this._size() +this._size() *0.5
+//     return posX
+//   },
+//
+//   _posY: function() {
+//     let posY
+//     let line
+//     line = Math.floor(this.num/GRIDSIZE)
+//     posY = line *this._size() +this._size() *0.5
+//     return posY
+//   },
+//
+//   _size: function() {
+//     let size
+//     size = width/GRIDSIZE/this.lvl
+//     return size
+//   },
+//
+//   __drawSharp: function(posX, posY, size) {
+//     rect(posX, posY, size*0.1, size*2, 8)
+//   },
+//
+//   __drawO: function(posX, posY, size) {
+//     ellipse(posX, posY, size*0.4, size*0.4)
+//     blendMode(DIFFERENCE)
+//     ellipse(posX, posY, size*0.2, size*0.2)
+//     blendMode(BLEND)
+//   },
+//
+//   __drawX: function(posX, posY, size) {
+//     push()
+//     translate(posX, posY +size*0.7)
+//     rotate(45)
+//     rect(-size*0.5, -size*0.5, size, size *0.2, 25)
+//     rect(-size*0.5, -size*0.5, size *0.2, size, 25)
+//     translate(posX, posY)
+//     pop()
+//   }
+// }
 
-let UbuntuMono
+
+
+function makeGrids(levels, innerGrids) {
+  if (levels > 1) {
+    for (let i = 0; i < GRIDSIZE**2; i++) {
+      let newGrid = ([LEVELS-levels+2])
+      innerGrids.push(newGrid)
+      makeGrids(levels -1, newGrid)
+    }
+  } else {
+    for (let i = 0; i < GRIDSIZE**2; i++) {
+      innerGrids.push(Math.floor(Math.random()*2)+1)
+    }
+  }
+}
+
+function drawGrids(arr, posX, posY, size, lvl = 1, idx = 0) {
+  let column = (idx -GRIDSIZE *Math.floor(idx/GRIDSIZE)) *size
+  let line = (Math.floor(idx/GRIDSIZE)) *size
+  let extg = size*(1/GRIDSIZE)*(lvl-1/LEVELS)*0.12
+
+  let innSize = size*(1/GRIDSIZE)*0.9
+
+  if (Array.isArray(arr)) {
+    for (let i = 1; i < arr.length; i++) {
+      drawSharp(posX +column +extg, posY +line +extg, size)
+      drawGrids(arr[i], posX +column +extg, posY +line +extg, innSize, lvl +1, (i-1))
+    }
+  } else {
+    // (lvl, idx+1)
+    // if (arr===0) // Click zone
+    if (arr===1) drawO(posX +column +extg, posY +line +extg, innSize)
+    if (arr===2) drawX(posX +column +extg, posY +line +extg, innSize)
+  }
+}
+
+function drawSharp(posX, posY, size) {
+  for (var i = 0; i < GRIDSIZE; i++) {
+    rect(posX+size*0.5, posY+size*0.5, size*0.03, size*0.85, 8)
+    rect(posX+size*0.5, posY+size*0.5, size*0.85, size*0.03, 8)
+  }
+}
+
+function drawO(posX, posY, size) {
+  ellipse(posX+size, posY+size, size*0.8, size*0.8)
+  blendMode(DIFFERENCE)
+  ellipse(posX+size, posY+size, size*0.4, size*0.4)
+  blendMode(BLEND)
+}
+
+function drawX(posX, posY, size) {
+  push()
+  translate(posX+size, posY+size +size*1.4)
+  rotate(45)
+  rect(-size, -size, size*1.8, size *0.4, 25)
+  rect(-size, -size, size *0.4, size*1.8, 25)
+  translate(posX+size, posY+size)
+  pop()
+}
 
 // eslint-disable-next-line
 function preload() {
-  UbuntuMono = loadFont('assets/UbuntuMono-Regular.ttf')
+  makeGrids(LEVELS, grids = ([1]))
+  // UbuntuMono = loadFont('assets/UbuntuMono-Regular.ttf')
+  setInterval(function() {
+    clock = clock < 500 ? clock +1 : 0
+  }, 1)
 }
 
 // eslint-disable-next-line
 function setup() {
-  // frameRate(1)
+  frameRate(1)
   createCanvas($(window).width(), $(window).height()).parent('game')
 
   noStroke()
   ellipseMode(RADIUS)
-  rectMode(RADIUS)
-  textFont(UbuntuMono)
+  rectMode(CENTER)
   colorMode(HSB, 360, 100, 100)
   angleMode(DEGREES)
   fill(100, 0, 92)
@@ -49,95 +183,8 @@ function draw() {
   fill(100, 0, 92)
   blendMode(BLEND)
 
-  drawGrid(outerGrid, width, height, 0, 0)
-}
-
-function Grid(options) {
-  this.lvl = 0
-  this.width = 0
-  this.height = 0
-  this.values = []
-
-  if (options) {
-    for (let prop in options) {
-      if (this.hasOwnProperty(prop)) {
-        this[prop] = options[prop]
-      }
-    }
-  }
-}
-
-function drawGrid(grid, width, height, posX, posY) {
-  let lvl = grid.lvl
-  let size = height > width ? width /grid.width : height /grid.height
-  let newline = height *0.5 -(size *grid.height *0.5)
-  let space = width *0.5 -(size *grid.width *0.5)
-  let value = -1
-
-  drawSharp(grid, width, height, size, posX, posY)
-
-  for (let i = 0; i < grid.height; i++) {
-    newline += i===0 ? 0 : size
-    for (let j = 0; j < grid.width; j++) {
-      value++
-      space += j===0 ? 0 : size
-
-      if (detectBlock(grid.values[value], posX +(size *0.5 +space), posY +(size *0.5+newline), size/2.4)) {
-        rect(posX +(size *0.5 +space), posY +(size *0.5+newline), size/2.4, size/2.4)
-      }
-
-      if (lvl===0) {
-        drawGrid(innerGrid, width*0.25, height*0.25, -width/8 +(size *0.5 +space), -height/8 +(size *0.5 +newline))
-      } else {
-        drawValue(grid.values[value], posX +(size *0.5 +space), posY +(size *0.5+newline), size/3)
-      }
-    }
-    space = width *0.5 -(size *grid.width *0.5)
-  }
-}
-
-function drawOs(posX, posY, radius) {
-  ellipse(posX, posY, radius, radius)
-  blendMode(DIFFERENCE)
-  ellipse(posX, posY, radius*0.8, radius*0.8)
-  blendMode(BLEND)
-}
-
-function drawXx(posX, posY, radius) {
-  push()
-  translate(posX, posY +radius*0.7)
-  rotate(45)
-  rect(-radius*0.5, -radius*0.5, radius, radius*0.2, 25)
-  rect(-radius*0.5, -radius*0.5, radius*0.2, radius, 25)
-  translate(posX, posY)
-  pop()
-}
-
-function drawValue(value, posX, posY, radius) {
-  switch (value) {
-    case 1: drawOs(posX, posY, radius); break
-    case 2: drawXx(posX, posY, radius); break
-    default:
-  }
-}
-
-function drawSharp(grid, width, height, size, posX, posY) {
-  let newline = height *0.5 -(size *grid.height *0.5)
-  let space = width *0.5 -(size *grid.width *0.5)
-
-  for (let i = 1; i < grid.width; i++) {
-    newline += i===1 ? 0 : size *grid.height
-      space += i===1 ? 0 : size *grid.width
-
-    rect(posX +((width +space) /grid.width), posY +(height *0.5), size*0.03, size*grid.height*0.45, 8) // verticals
-    rect(posX +(width *0.5), posY +((height +newline) /grid.height), size*grid.width*0.45, size*0.03, 8) // horizontals
-  }
-}
-
-function detectBlock(grid, posX, posY, size) {
-  if (mouseX > posX -size && mouseX < posX +size) {
-    if (mouseY > posY -size && mouseY < posY +size) {
-      return true
-    }
-  } else return
+  let size = $(window).width() > $(window).height() ? $(window).height() : $(window).width()
+  let posX = $(window).width() > $(window).height() ? $(window).width()*0.5-size*0.5 : 0
+  let posY = $(window).width() > $(window).height() ? 0 : $(window).height()*0.5-size*0.5
+  drawGrids(grids, posX, posY, size)
 }
