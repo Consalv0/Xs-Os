c54687346c65d1faef958f00082019e914362628
Consalvo <gonzaloceciliano@gmail.com>
2017-04-12 18:18:47 -0500

    Added Zoom

--- js/game.js
+++ js/game.js
@@ -1,100 +1,22 @@
 /* eslint space-infix-ops: 0, space-before-function-paren: 0, indent: 0, no-trailing-spaces: 0 */
 /* global $, createCanvas, loadFont, colorMode, blendMode, fill, text, textFont, textSize, DIFFERENCE, HSB, width,
    ellipse, ellipseMode, RADIUS, rect, rectMode, rotate, noStroke, height, BLEND, push, translate,
-   pop, angleMode, DEGREES, mouseX, mouseY, CENTER */
+   pop, angleMode, DEGREES, mouseX, mouseY, CENTER, pmouseX, pmouseY, map */
 
 // let UbuntuMono
 let grids = []
 let mouseIsCliked = false
-let zoom
+let zoom = 1
+let zPos = ({x: 0, y: 0})
+let _posX
+let _posY
+let _size
 let clock = 0
-let GRIDSIZE = 2
+let GRIDSIZE = 3
 let LEVELS = 2
 
-// function Grid(options) {
-//   this.lvl = LEVELS
-//   this.width = width
-//   this.height = height
-//   // this.values = []
-//
-//   if (options) {
-//     for (let prop in options) {
-//       if (this.hasOwnProperty(prop)) {
-//         this[prop] = options[prop]
-//       }
-//     }
-//   }
-//
-//   this.values = []
-//   for (let i = 0; i < GRIDSIZE**2; i++) {
-//     if (this.lvl <= LEVELS-1) {
-//       // this.values.push(Math.ceil(Math.random()*3))
-//       this.values.push(3)
-//     } else {
-//       this.values.push(Math.floor(Math.random()*3))
-//     }
-//   }
-// }
-//
-// Grid.prototype = {
-//
-//   draw: function(lvl, num) {
-//     this.__drawSharp(this._posX(), this._posY(), this._size())
-//     for (var i = 0; i < this.values.length; i++) {
-//       if (this.values[i] === 1) {
-//         this.__drawX(this._posX(), this._posY(), this._size())
-//       }
-//       if (this.values[i] === 2) {
-//         this.__drawO(this._posX(), this._posY(), this._size())
-//       }
-//     }
-//   },
-//
-//   _posX: function() {
-//     let posX
-//     let colum
-//     colum = this.num-GRIDSIZE*Math.floor(this.num/GRIDSIZE)
-//     posX = colum *this._size() +this._size() *0.5
-//     return posX
-//   },
-//
-//   _posY: function() {
-//     let posY
-//     let line
-//     line = Math.floor(this.num/GRIDSIZE)
-//     posY = line *this._size() +this._size() *0.5
-//     return posY
-//   },
-//
-//   _size: function() {
-//     let size
-//     size = width/GRIDSIZE/this.lvl
-//     return size
-//   },
-//
-//   __drawSharp: function(posX, posY, size) {
-//     rect(posX, posY, size*0.1, size*2, 8)
-//   },
-//
-//   __drawO: function(posX, posY, size) {
-//     ellipse(posX, posY, size*0.4, size*0.4)
-//     blendMode(DIFFERENCE)
-//     ellipse(posX, posY, size*0.2, size*0.2)
-//     blendMode(BLEND)
-//   },
-//
-//   __drawX: function(posX, posY, size) {
-//     push()
-//     translate(posX, posY +size*0.7)
-//     rotate(45)
-//     rect(-size*0.5, -size*0.5, size, size *0.2, 25)
-//     rect(-size*0.5, -size*0.5, size *0.2, size, 25)
-//     translate(posX, posY)
-//     pop()
-//   }
-// }
-
-
+let mapX = 0
+let mapY = 0
 
 function makeGrids(levels, innerGrids) {
   if (levels > 1) {
@@ -115,42 +37,48 @@ function drawGrids(arr, posX, posY, size, lvl = 1, idx = 0) {
   let line = (Math.floor(idx/GRIDSIZE)) *size
 
   let innSize = size*(1/GRIDSIZE)*0.9
-  let extg = (1/size)*(1/GRIDSIZE)*(1/LEVELS)
 
   if (Array.isArray(arr)) {
     for (let i = 1; i < arr.length; i++) {
-      drawSharp(posX +column +extg, posY +line +extg, size)
-      drawGrids(arr[i], posX +column +extg, posY +line +extg, innSize, lvl +1, (i-1))
+      drawSharp(posX +column, posY +line, size, innSize)
+      drawGrids(arr[i], posX +column, posY +line, innSize, lvl +1, (i-1))
     }
   } else {
     // (lvl, idx+1)
     // if (arr===0) // Click zone
-    if (arr===1) drawO(posX +column +extg, posY +line +extg, innSize)
-    if (arr===2) drawX(posX +column +extg, posY +line +extg, innSize)
+    if (arr===1) drawO(posX +column, posY +line, innSize)
+    if (arr===2) drawX(posX +column, posY +line, innSize)
   }
 }
 
-function drawSharp(posX, posY, size) {
-  for (var i = 0; i < GRIDSIZE; i++) {
-    rect(posX+size*0.425, posY+size*0.425, size*0.03, size*0.85, 8)
-    rect(posX+size*0.425, posY+size*0.425, size*0.85, size*0.03, 8)
+function drawSharp(posX, posY, size, innSize) {
+  let extg = 1/(GRIDSIZE+extgThing())
+  for (var i = 1; i < GRIDSIZE; i += 1.08) {
+    rect(posX +innSize*i -innSize*extg*i,
+         posY +innSize*(GRIDSIZE*0.5) -innSize*GRIDSIZE*0.25*extg, size*0.03, size*0.8, 8)
+
+    rect(posX +innSize*(GRIDSIZE*0.5) -innSize*GRIDSIZE*0.25*extg,
+         posY +innSize*i -innSize*extg*i, size*0.8, size*0.03, 8)
   }
 }
 
 function drawO(posX, posY, size) {
-  ellipse(posX+size, posY+size, size*0.8, size*0.8)
+  let extg = size+size*GRIDSIZE*1.5*(1/(GRIDSIZE+extgThing()))
+  // rect(posX+extg, posY+extg, size*2, size*2)
+  ellipse(posX+extg, posY+extg, size*0.8, size*0.8)
   blendMode(DIFFERENCE)
-  ellipse(posX+size, posY+size, size*0.4, size*0.4)
+  ellipse(posX+extg, posY+extg, size*0.4, size*0.4)
   blendMode(BLEND)
 }
 
 function drawX(posX, posY, size) {
+  let extg = size+size*GRIDSIZE*1.5*(1/(GRIDSIZE+extgThing()))
   push()
-  translate(posX+size, posY+size +size*1.4)
+  translate(posX+extg, posY+extg +size*1.4)
   rotate(45)
   rect(-size, -size, size*1.8, size *0.4, 25)
   rect(-size, -size, size *0.4, size*1.8, 25)
-  translate(posX+size, posY+size)
+  translate(posX+extg, posY+extg)
   pop()
 }
 
@@ -181,7 +109,7 @@ function preload() {
 
 // eslint-disable-next-line
 function setup() {
-  frameRate(1)
+  // frameRate(1)
   createCanvas($(window).width(), $(window).height()).parent('game')
 
   noStroke()
@@ -195,17 +123,43 @@ function setup() {
 // eslint-disable-next-line
 function draw() {
   fill(0, 100, 0)
-  rect(width/2, height/2, width, height)
+  rect(width*0.5, height*0.5, width, height)
   fill(100, 0, 92)
   blendMode(BLEND)
 
-  let size = $(window).width() > $(window).height() ? $(window).height() : $(window).width()
-  let posX = $(window).width() > $(window).height()
-    ? $(window).width()*0.5 -size*0.5 +size*(1/(GRIDSIZE+extgThing()))
-    : size*(1/(GRIDSIZE+extgThing()))
-  let posY = $(window).width() > $(window).height()
-    ? size*(1/(GRIDSIZE+extgThing()))
-    : $(window).height()*0.5 -size*0.5 +size*(1/(GRIDSIZE+extgThing()))
+  _size = $(window).width() > $(window).height() ? $(window).height()*zoom : $(window).width()*zoom
+  _posX = $(window).width() > $(window).height()
+    ? $(window).width()*0.5 -_size*0.5 +_size*(1/(GRIDSIZE+extgThing()))*zoom -mapX*(_size/GRIDSIZE) +mapX*(_size*0.1)
+    : _size*(1/(GRIDSIZE+extgThing()))*(1/zoom) -mapX*(_size/GRIDSIZE) +mapX*(_size*0.1)
+  _posY = $(window).width() > $(window).height()
+    ? _size*(1/(GRIDSIZE+extgThing()))*(1/zoom) -mapY*(_size/GRIDSIZE) +mapY*(_size*0.1)
+    : $(window).height()*0.5 -_size*0.5 +_size*(1/(GRIDSIZE+extgThing()))*zoom -mapY*(_size/GRIDSIZE) +mapY*(_size*0.1)
 
-  drawGrids(grids, posX, posY, size)
+  drawGrids(grids, _posX, _posY, _size)
+}
+
+// eslint-disable-next-line
+function touchEnded() {
+  console.log(mapX, mapY)
+  if (zoom > 1) {
+    mapX = 0
+    mapY = 0
+    zoom = 1
+    zPos = ({
+      x: 0,
+      y: 0
+    })
+  } else {
+    if (pmouseX > _posX && pmouseX < _posX+(_size*0.85)) {
+      if (pmouseY > _posY && pmouseY < _posY+(_size*0.85)) {
+        mapX = Math.floor(map(pmouseX, +_posX, _size*0.85 +_posX, 0, GRIDSIZE))
+        mapY = Math.floor(map(pmouseY, +_posY, _size*0.85 +_posY, 0, GRIDSIZE))
+        zoom = zoom*2
+        zPos = ({
+          x: mapX,
+          y: mapY
+        })
+      }
+    }
+  }
 }
